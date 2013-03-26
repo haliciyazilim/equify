07ab69468ea5a8206d1ed14dd33f9971ea7b051c
Alperen Kavun <alperen@halici.com.tr>
2013-03-26 15:18:12 +0200

    Added bestFifteen average leaderbord Ids

--- Equify/GameCenter/EQGameCenterSpecificValues.h
+++ Equify/GameCenter/EQGameCenterSpecificValues.h
@@ -9,12 +9,15 @@
 // GameCenter leaderboards
 #define kLeaderboardBestTime @"com.halici.Equify.leaderboards.bestTime1"
 #define kLeaderboardTotalSolvedQuestion @"com.halici.Equify.leaderboards.totalSolvedQuestion1"
+#define kLeaderboardFifteenAverage @"com.halici.Equify.leaderboards.bestFifteen1"
 
 #define kLeaderboardBestTime2 @"com.halici.Equify.leaderboards.bestTime2"
 #define kLeaderboardTotalSolvedQuestion2 @"com.halici.Equify.leaderboards.totalSolvedQuestion2"
+#define kLeaderboardFifteenAverage2 @"com.halici.Equify.leaderboards.bestFifteen1"
 
 #define kLeaderboardBestTime3 @"com.halici.Equify.leaderboards.bestTime3"
 #define kLeaderboardTotalSolvedQuestion3 @"com.halici.Equify.leaderboards.totalSolvedQuestion3"
+#define kLeaderboardFifteenAverage3 @"com.halici.Equify.leaderboards.bestFifteen3"
 
 // GameCenter Notifications
 #define kAuthenticationChangedNotification @"AuthenticedChanged"
