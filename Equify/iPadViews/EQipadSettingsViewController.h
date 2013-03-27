7a6cbd956b3faa9514dd216a5c2a375b1268bf81
edebimce <abdullah@halici.com.tr>
2013-03-27 13:37:58 +0200

    ipad configuraiton - new controller added

---
+++ Equify/iPadViews/EQipadSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  EQipadSettingsViewController.h
+//  Equify
+//
+//  Created by Abdullah Karacabey on 27.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import "EQSettingsViewController.h"
+
+@interface EQipadSettingsViewController : EQSettingsViewController
+
+@end
---
+++ Equify/iPadViews/EQipadSettingsViewController.m
@@ -0,0 +1,19 @@
+//
+//  EQipadSettingsViewController.m
+//  Equify
+//
+//  Created by Abdullah Karacabey on 27.03.2013.
+//  Copyright (c) 2013 Halıcı. All rights reserved.
+//
+
+#import "EQipadSettingsViewController.h"
+
+@interface EQipadSettingsViewController ()
+
+@end
+
+@implementation EQipadSettingsViewController
+
+
+
+@end
